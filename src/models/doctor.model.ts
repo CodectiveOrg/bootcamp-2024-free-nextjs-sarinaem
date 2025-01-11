934ba43084bee8540d17ce8031b8e8e4e14dbc8b
sarina <sarinaem75@gmail.com>
2025-01-12 01:55:00 +0330

    [ADD] mock data

---
+++ src/models/doctor.model.ts
@@ -0,0 +1,15 @@
+export type DoctorModel = {
+  id?: string;
+  name?: string;
+  image?: string;
+  gender?: "آقا" | "خانم";
+  averageRating: string | number;
+  isVerified: boolean;
+  degree?: string;
+  address?: string;
+  badges?: string[];
+  totalVotes: number;
+  firstAvailableAppointment: string;
+  brief: string;
+  expertise: string;
+};
---
+++ src/types/filter.type.ts
@@ -0,0 +1,6 @@
+export type FilterType = {
+  gender?: "man" | "woman" | "other";
+  description?: string;
+  query?: string;
+  degree?: "bachelor" | "senior" | "Ph.D" | "specialist";
+};
