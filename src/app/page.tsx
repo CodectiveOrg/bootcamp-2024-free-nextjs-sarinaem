a63c76f0ce2e73dfe4d915e501678a94270c87e6
sarina <sarinaem75@gmail.com>
2025-01-17 02:41:40 +0330

    [change] path logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 
 import GlobalSearchBoxComponent from "@/components/global-search-box/GlobalSearchBox.component";
 
-import DoctorFinder from "@/logo/DoctorFinder";
+import DoctorFinder from "@/logo/doctor-logo";
 
 import styles from "./page.module.css";
 
