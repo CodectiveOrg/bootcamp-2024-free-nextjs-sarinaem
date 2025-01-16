cefbb39a74278f97d50b9267784c6989cb82364b
sarina <sarinaem75@gmail.com>
2025-01-17 02:08:07 +0330

    [change] logo

--- src/logo/Doctorfinder.tsx
+++ src/logo/Doctorfinder.tsx
@@ -1,6 +1,6 @@
 import React, { SVGProps } from "react";
 
-export function DoctorFinder(props: SVGProps<SVGSVGElement>) {
+function DoctorFinder(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
