5a18758e82a3a899a29394f75d49d354a4016717
sarinaem <56018924+sarinaem@users.noreply.github.com>
2025-01-26 23:55:11 +0330

    Update src/app/changeTheme/ChangeTheme.tsx
    Co-authored-by: BijanProgrammer <40728713+BijanProgrammer@users.noreply.github.com>

--- src/app/changeTheme/ChangeTheme.tsx
+++ src/app/changeTheme/ChangeTheme.tsx
@@ -29,8 +29,7 @@ const ChangeTheme = () => {
   }, [theme]);
 
   const toggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
+    setTheme((old) => old === "light" ? "dark" : "light");
   };
 
   return (
